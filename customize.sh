62f7b9194f993e9ddd8d39ea4518c26b053f054d
zhuqin <zhuqin83@gmail.com>
2021-11-25 12:25:45 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -4,7 +4,7 @@ shopt -s extglob
 sed -i '/	refresh_config();/d' scripts/feeds
 
 #1. Modify default IP
-#sed -i 's/192.168.1.1/192.168.1.252/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.252/g' openwrt/package/base-files/files/bin/config_generate
 
 #2. Custom settings
 #sed -i 's?zstd$?zstd ucl upx\n$(curdir)/upx/compile := $(curdir)/ucl/compile?g' tools/Makefile
