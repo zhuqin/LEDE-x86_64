062cdf57d39eaf493f63bc53b9837a14aa04313c
zhuqin <zhuqin83@gmail.com>
2024-10-24 19:29:11 +0800

    update default theme

--- customize.sh
+++ customize.sh
@@ -4,7 +4,7 @@
 sed -i 's/192.168.1.1/192.168.1.252/g' openwrt/package/base-files/files/bin/config_generate
 
 # Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-design/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
 #sed -i 's/OpenWrt/kenzo/g' package/base-files/files/bin/config_generate
