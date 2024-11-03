3d1d369c6f6a118deb515fea18d669bc9ad2c40b
zhuqin <zhuqin83@gmail.com>
2024-11-03 11:05:05 +0800

    update customize

--- customize.sh
+++ customize.sh
@@ -4,10 +4,10 @@
 sed -i 's/192.168.1.1/192.168.1.252/g' openwrt/package/base-files/files/bin/config_generate
 
 # Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-design/g' feeds/luci/collections/luci/Makefile
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-#sed -i 's/OpenWrt/kenzo/g' package/base-files/files/bin/config_generate
+#sed -i 's/OpenWrt/kenzo/g' openwrt/package/base-files/files/bin/config_generate
 
 #2. Custom settings
 #sed -i 's?zstd$?zstd ucl upx\n$(curdir)/upx/compile := $(curdir)/ucl/compile?g' tools/Makefile
